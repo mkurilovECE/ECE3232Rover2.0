d81dd3d237bba3202580a739a81f93119f3c9aea
mfloyd2CS1003 <90395278+Mattias-Floyd@users.noreply.github.com>
2024-04-05 15:16:26 -0300

    Update MotorVector.c
    undid or logic change for check

--- ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
+++ ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
@@ -21,7 +21,7 @@
 
 int motorvectorleft(int powervec, int steeringvec) {
     int outleft = 0;
-    if ((0 <= powervec || powervec <= 100) && (0 <= steeringvec || steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+    if ((0 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
         if (steeringvec > 50){ // this is for right turns
         
             outleft = (powervec/50) * (100 - steeringvec) * 2; //scales the left motor to go from full speed at center to stopped at far right
@@ -43,7 +43,7 @@ int motorvectorleft(int powervec, int steeringvec) {
 
 int motorvectorright(int powervec, int steeringvec) {
     int outright =0;
-    if ((0 <= powervec || powervec <= 100) && (0 <= steeringvec || steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+    if ((0 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
         if (steeringvec > 50){ // this is for right turns
       
             outright = (powervec/50) * (steeringvec) *2;
