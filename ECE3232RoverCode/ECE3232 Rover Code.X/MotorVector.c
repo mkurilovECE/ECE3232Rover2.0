58c888112af32c4782fb1596b7ba5b474e315bca
mfloyd2CS1003 <90395278+Mattias-Floyd@users.noreply.github.com>
2024-04-07 20:11:59 -0300

    Update MotorVector.c
    This accounds for forwards and backwards. Direction function added

--- ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
+++ ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
@@ -10,18 +10,24 @@
 #include "MotorVector.h"
 
 
+
 //This function take in a steering vector (0-100) and a power vector and converts them into a proportionality
 //for power to left and right 
 
 
 //for powervec, we need an input 0-100 (0 is backwards full, 50 is stopped and 100 is full forwards)
 //for steeringvec, we need an input 0-100 (0 is left, 50 is straight and 100 is right)
+//this code does not do tank style turning, it does pivot turning 
 
-
+int abs(int v) 
+{
+  return v * ((v>0) - (v<0));
+}
 
 int motorvectorleft(int powervec, int steeringvec) {
-    int outleft = 0;
-    if ((0 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+    volatile int outleft = 0;
+    powervec = abs(powervec-50); //this takes direction out of the equation (now a positive scaler value 1-50)
+    if ((0 <= powervec && powervec <= 50) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
         if (steeringvec > 50){ // this is for right turns
         
             outleft = (powervec/50) * (100 - steeringvec) * 2; //scales the left motor to go from full speed at center to stopped at far right
@@ -42,8 +48,9 @@ int motorvectorleft(int powervec, int steeringvec) {
 }
 
 int motorvectorright(int powervec, int steeringvec) {
-    int outright =0;
-    if ((0 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+    powervec = abs(powervec-50);
+    volatile int outright =0;
+    if ((0 <= powervec && powervec <= 50) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
         if (steeringvec > 50){ // this is for right turns
       
             outright = (powervec/50) * (steeringvec) *2;
@@ -60,4 +67,19 @@ int motorvectorright(int powervec, int steeringvec) {
     
     }
     return outright;
+}
+
+int direction(int powervec){
+    volatile int direction = 0;
+    if (powervec > 50) {
+        direction = 1; //forwards
+    
+    } else if (powervec < 50 ){
+        direction = 2; //backwards
+    } else {
+    
+        direction = 0; //brake
+    }
+
+    return direction;
 }
