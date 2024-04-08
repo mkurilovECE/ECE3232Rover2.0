b9ec44ae2f232e5cfbf2a1c3a2fd0885057fa7af
mfloyd2CS1003 <90395278+Mattias-Floyd@users.noreply.github.com>
2024-04-08 18:13:39 -0300

    Update MotorVector.c
    my code again lol

--- ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
+++ ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
@@ -1,4 +1,3 @@
-
 /*
  * File:   MotorVector.c
  * Author: Mattias Floyd
@@ -20,40 +19,67 @@
 //for steeringvec, we need an input 0-100 (0 is left, 50 is straight and 100 is right)
 //this code does not do tank style turning, it does pivot turning 
 
-
+int abs(int v) 
+{
+  return v * ((v>0) - (v<0));
+}
 
 int motorvectorleft(int powervec, int steeringvec) {
-    int outleft = 0;
-    
-    if ((50 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+    volatile int outleft = 0;
+    powervec = abs(powervec-50); //this takes direction out of the equation (now a positive scaler value 1-50)
+    if ((0 <= powervec && powervec <= 50) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+        if (steeringvec > 50){ // this is for right turns
         
-        
-             //scales the left motor to go from full speed at center to stopped at far right
+            outleft = (powervec/50) * (100 - steeringvec) * 2; //scales the left motor to go from full speed at center to stopped at far right
             
-            outleft = (powervec/100) * (steeringvec);
-       if (steeringvec<55 && steeringvec>45)  // this means stick is in the center and we just want it to go as fast as we push the stick straight
+            
+        } else if(steeringvec < 50){ //this is for left turns
+        
+            outleft = (powervec/50) * (steeringvec) * 2;
+         
+        
+        } else {  // this means stick is in the center and we just want it to go as fast as we push the stick straight
             outleft = powervec;
           
         }
     
-    
+    }
     return outleft;
 }
 
 int motorvectorright(int powervec, int steeringvec) {
-    int outright =0;
-    if ((50 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
-       
+    powervec = abs(powervec-50);
+    volatile int outright =0;
+    if ((0 <= powervec && powervec <= 50) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+        if (steeringvec > 50){ // this is for right turns
       
-           outright = (powervec/100) * (100 - steeringvec);
+            outright = (powervec/50) * (steeringvec) *2;
             
-            
+        } else if(steeringvec < 50){ //this is for left turns
         
-          if (steeringvec<55 && steeringvec>45)  // this means stick is in the center and we just want it to go as fast as we push the stick straight
+           
+            outright = (powervec/50) * (100 - steeringvec) *2;
+        
+        } else {  // this means stick is in the center and we just want it to go as fast as we push the stick straight
+            
             outright = powervec;
-          
         }
     
-    
+    }
     return outright;
+}
+
+int direction(int powervec){
+    volatile int direction = 0;
+    if (powervec > 50) {
+        direction = 1; //forwards
+    
+    } else if (powervec < 50 ){
+        direction = 2; //backwards
+    } else {
+    
+        direction = 0; //brake
+    }
+
+    return direction;
 }
