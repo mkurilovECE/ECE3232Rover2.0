ae955143968a47440dfd25fdf451d0eafbba59fc
mferris3CS1003 <90429215+mferris3CS1003@users.noreply.github.com>
2024-04-08 14:36:44 -0300

    Update MotorVector.c

--- ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
+++ ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
@@ -22,18 +22,18 @@
 
 
 int motorvectorleft(int powervec, int steeringvec) {
-    volatile int outleft = 0;
+    int outleft = 0;
     
     if ((50 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
         if (steeringvec > 50){ // this is for right turns
         
              //scales the left motor to go from full speed at center to stopped at far right
             
-            outleft = (powervec/200) * (steeringvec);
+            outleft = 2*(powervec/200) * (steeringvec);
         } else if(steeringvec < 50){ //this is for left turns
         
             
-         outleft = (powervec/100) * (100 - steeringvec);
+         outleft = 2*(powervec/100) * (100 - steeringvec);
         
         } else {  // this means stick is in the center and we just want it to go as fast as we push the stick straight
             outleft = powervec;
@@ -45,15 +45,15 @@ int motorvectorleft(int powervec, int steeringvec) {
 }
 
 int motorvectorright(int powervec, int steeringvec) {
-    volatile int outright =0;
+     int outright =0;
     if ((50 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
         if (steeringvec > 50){ // this is for right turns
       
-           outright = (powervec/100) * (100 - steeringvec);
+           outright = 2*(powervec/100) * (100 - steeringvec);
             
         } else if(steeringvec < 50){ //this is for left turns
         
-            outright = (powervec/200) * (steeringvec);
+            outright = 2*(powervec/200) * (steeringvec);
             
         
         } else {  // this means stick is in the center and we just want it to go as fast as we push the stick straight
