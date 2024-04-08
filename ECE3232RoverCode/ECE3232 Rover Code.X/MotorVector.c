e39c3b623e260c6053874499d6ffe450dd57dc81
mferris3CS1003 <90429215+mferris3CS1003@users.noreply.github.com>
2024-04-08 13:44:31 -0300

    Update MotorVector.c

--- ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
+++ ECE3232RoverCode/ECE3232 Rover Code.X/MotorVector.c
@@ -11,81 +11,56 @@
 
 
 
- //This function take in a steering vector (0-100) and a power vector and converts them into a proportionality
- //for power to left and right 
+//This function take in a steering vector (0-100) and a power vector and converts them into a proportionality
+//for power to left and right 
 
 
- //for powervec, we need an input 0-100 (0 is backwards full, 50 is stopped and 100 is full forwards)
- //for steeringvec, we need an input 0-100 (0 is left, 50 is straight and 100 is right)
- //this code does not do tank style turning, it does pivot turning 
+//for powervec, we need an input 0-100 (0 is backwards full, 50 is stopped and 100 is full forwards)
+//for steeringvec, we need an input 0-100 (0 is left, 50 is straight and 100 is right)
+//this code does not do tank style turning, it does pivot turning 
 
-int abs(int v)
-{
-    return v * ((v > 0) - (v < 0));
-}
+
 
 int motorvectorleft(int powervec, int steeringvec) {
     int outleft = 0;
-    //powervec = abs(powervec-50); //this takes direction out of the equation (now a positive scaler value 1-50)
-    if ((0 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)) {  //makes sure both values are in range (doesn't do anything if they aren't)
-        if (steeringvec > 50) { // this is for right turns
-
-            outleft = (powervec / 50) * (100 - steeringvec) * 2; //scales the left motor to go from full speed at center to stopped at far right
-
-
-        }
-        else if (steeringvec < 50) { //this is for left turns
-
-            outleft = (powervec / 50) * (steeringvec) * 2;
-
-
-        }
-        else {  // this means stick is in the center and we just want it to go as fast as we push the stick straight
+    
+    if ((50 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+        if (steeringvec > 50){ // this is for right turns
+        
+            outleft = (powervec/100) * (100 - steeringvec); //scales the left motor to go from full speed at center to stopped at far right
+            
+            
+        } else if(steeringvec < 50){ //this is for left turns
+        
+            outleft = (powervec/200) * (steeringvec);
+         
+        
+        } else {  // this means stick is in the center and we just want it to go as fast as we push the stick straight
             outleft = powervec;
-
+          
         }
-
+    
     }
     return outleft;
 }
 
 int motorvectorright(int powervec, int steeringvec) {
-    //powervec = abs(powervec-50);
-    int outright = 0;
-    if ((0 <= powervec && powervec <= 50) && (0 <= steeringvec && steeringvec <= 100)) {  //makes sure both values are in range (doesn't do anything if they aren't)
-        if (steeringvec > 50) { // this is for right turns
-
-            outright = (powervec / 50) * (steeringvec) * 2;
-
-        }
-        else if (steeringvec < 50) { //this is for left turns
-
-
-            outright = (powervec / 50) * (100 - steeringvec) * 2;
-
-        }
-        else {  // this means stick is in the center and we just want it to go as fast as we push the stick straight
-
+    int outright =0;
+    if ((50 <= powervec && powervec <= 100) && (0 <= steeringvec && steeringvec <= 100)){  //makes sure both values are in range (doesn't do anything if they aren't)
+        if (steeringvec > 50){ // this is for right turns
+      
+            outright = (powervec/200) * (steeringvec);
+            
+        } else if(steeringvec < 50){ //this is for left turns
+        
+           
+            outright = (powervec/100) * (100 - steeringvec);
+        
+        } else {  // this means stick is in the center and we just want it to go as fast as we push the stick straight
+            
             outright = powervec;
         }
-
+    
     }
     return outright;
-}
-
-int direction(int powervec) {
-    volatile int direction = 0;
-    if (powervec > 50) {
-        direction = 1; //forwards
-
-    }
-    else if (powervec < 50) {
-        direction = 2; //backwards
-    }
-    else {
-
-        direction = 0; //brake
-    }
-
-    return direction;
 }
